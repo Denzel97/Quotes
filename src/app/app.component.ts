e8d5165d157315da1b05491696a7862b9b9a7bf6
Denzel97 <denzelwash97@gmail.com>
2019-01-12 16:04:44 +0300

    add funcionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,9 @@
 import { Component } from '@angular/core';
-import {Quote} from './quote'
+import { Quote } from './quote'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  quotes = [
-        new Quote(1,'Treat me like a joke and I will leave you like it is funny'),
-        new Quote(2,'I am not wierd I am limited edition'),
-        new Quote(3, 'Get up, stand up, Stand up for your rights. Get up, stand up, Don not give up the fight'),
-        new Quote(4, 'Talk is cheap. Show me code'),
-        new Quote(5, 'Things are not always #000000 and #FFFFFF'),
-        new Quote(6, 'Eat Sleep Game Repeat'),
-        new Quote(7, 'Gamers do not die they respawn'),
-
-
-    ]
-  constructor() {}
-}
+export class AppComponent { }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,25 +1,25 @@
-import { Component } from '@angular/core';
-import {Quote} from './quote'
+import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote'
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-quote',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   quotes = [
-        new Quote(1,'Treat me like a joke and I will leave you like it is funny'),
-        new Quote(2,'I am not wierd I am limited edition'),
-        new Quote(3, 'Get up, stand up, Stand up for your rights. Get up, stand up, Don not give up the fight'),
-        new Quote(4, 'Talk is cheap. Show me code'),
-        new Quote(5, 'Things are not always #000000 and #FFFFFF'),
-        new Quote(6, 'Eat Sleep Game Repeat'),
-        new Quote(7, 'Gamers do not die they respawn'),
+    new Quote(1, 'Treat me like a joke and I will leave you like it is funny', 'By Chelsea Lydia'),
+    new Quote(2, 'I am not wierd I am limited edition', 'By Abby'),
+    new Quote(3, 'Get up, stand up, Stand up for your rights. Get up, stand up, Don not give up the fight', 'By Bob Marley'),
+    new Quote(4, 'Talk is cheap. Show me code', 'By June Barasa'),
+    new Quote(5, 'Things are not always #000000 and #FFFFFF', 'By Yommie Samora'),
+    new Quote(6, 'Eat Sleep Game Repeat', 'Benson Kimani'),
+    new Quote(7, 'Gamers do not die they respawn', 'By Denzel Ouma'),
 
 
-    ]
-  constructor() {}
-  
+  ]
+  constructor() { }
+
   ngOnInit() {
   }
 
