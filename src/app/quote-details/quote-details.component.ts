7adc5d7a71a87115477e93300a06e1b8a29dd865
Denzel97 <denzelwash97@gmail.com>
2019-01-14 18:18:01 +0300

    add like and unlike functionality

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -11,6 +11,17 @@ export class QuoteDetailsComponent implements OnInit {
   @Input() quote: Quote;
   @Output() isComplete = new EventEmitter<boolean>();
 
+  upvote = 0;
+  downvote = 0;
+
+  like() {
+    this.upvote = this.upvote + 1;
+  }
+  unlike() {
+    this.downvote = this.downvote + 1;
+  }
+
+
   quoteDelete(complete: boolean) {
     this.isComplete.emit(complete);
   }
